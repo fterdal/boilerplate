2c966c6bba0afcb7e86d3429e6a5378d2a65400a
Finn Terdal <fterdal@gmail.com>
2017-09-11 11:01:33 -0500

    Fussing around with webpacl

--- client/index.jsx
+++ client/index.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-  <div>Hello, world!</div>,
+  <h1>Hello, world!</h1>,
   document.getElementById('app') // make sure this is the same as the id of the div in your index.html
 );
